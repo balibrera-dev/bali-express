d287c5081c96f405f9c17bc0b29c1a94b5d266b2
Rodrigo Balibrera <balibrera.dev@gmail.com>
2024-10-07 01:33:23 -0600

    add origin

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,6 +1,6 @@
 // CORS middleware
 const allowCrossDomain = (req, res, next) => {
-  res.header(`Access-Control-Allow-Origin`, `*`);
+  res.header(`Access-Control-Allow-Origin`, `http://localhost:3000`);
   res.header(`Access-Control-Allow-Methods`, `GET,PATCH,POST,DELETE`);
   res.header(`Access-Control-Allow-Headers`, `Content-Type`);
   next();
